a7cd5ff39a2e3f40f441a20438cb44bfcae2637e
rainac <johannes@johannes-willkomm.de>
2015-07-01 23:57:10 +0200

    Adding two new tests (failing) which test XML output indentation

--- tests/sh/p2x_tests.sh
+++ tests/sh/p2x_tests.sh
@@ -1,5 +1,9 @@
-#! /bin/bash
+#! /bin/zsh
 
+zmodload zsh/mathfunc
+set -o shwordsplit
+echo $0
+export SHUNIT_PARENT=$0
 #set -x
 
 testP2X1() {
@@ -55,6 +59,73 @@ testP2X5() {
 testP2X6() {
     p2x -i ../../examples/in/mult3.exp
     assertNotEquals "P2X should fail in this case" $? 0
+}
+
+
+# Test section indent
+testP2X_indentation() {
+
+    for kb in 1 2 3; do
+
+        IN_KB=$kb P2XOPTS=--indent ./speed_test.sh > /dev/null 2> /dev/null
+        ls -lrt /tmp/p2x-out.xml
+        sz_noindent_sm=$(ls -l /tmp/p2x-out.xml | cut -d ' ' -f 5)
+        IN_KB=$kb P2XOPTS=--indent-unit=" " ./speed_test.sh > /dev/null 2> /dev/null
+        ls -lrt /tmp/p2x-out.xml
+        sz_indent_sm=$(ls -l /tmp/p2x-out.xml | cut -d ' ' -f 5)
+        IN_KB=$kb P2XOPTS=--indent-unit="  " ./speed_test.sh > /dev/null 2> /dev/null
+        ls -lrt /tmp/p2x-out.xml
+        sz_indent_sm2=$(ls -l /tmp/p2x-out.xml | cut -d ' ' -f 5)
+
+        let  incr=$(( (1.0*$sz_indent_sm)/$sz_noindent_sm ))
+        let incr2=$(( (1.0*$sz_indent_sm2)/$sz_indent_sm  ))
+
+        echo ""
+        echo "*"
+        echo "indenting increases file size by a factor of $incr"
+        echo "indenting by twice the amount increases file size by a factor of $incr2"
+        echo "*"
+        echo ""
+
+        test 1 -eq $(( $incr > 1 ))
+        assertEquals "Indenting should increase file size" "0" "$?"
+
+        test 1 -eq $(( $incr2 > 1 ))
+        assertEquals "Indenting by twice the amount should increase file size by a factor approaching 2" "0" "$?"
+
+        test 1 -eq $(( $incr2 < 2 ))
+        assertEquals "Indenting by twice the amount should increase file size by a factor approaching 2" "0" "$?"
+
+        rm /tmp/p2x-speed-in
+        rm /tmp/p2x-out.xml
+    done
+}
+
+testP2_indent_not_too_bad() {
+
+    for kb in 1 10 20; do
+
+        IN_KB=$kb P2XOPTS=--indent ./speed_test.sh > /dev/null 2> /dev/null
+        ls -lrt /tmp/p2x-out.xml
+        sz_noindent_sm=$(ls -l /tmp/p2x-out.xml | cut -d ' ' -f 5)
+        IN_KB=$kb ./speed_test.sh > /dev/null 2> /dev/null
+        ls -lrt /tmp/p2x-out.xml
+        sz_indent_sm=$(ls -l /tmp/p2x-out.xml | cut -d ' ' -f 5)
+
+        let incr=$sz_indent_sm/$sz_noindent_sm
+
+        echo ""
+        echo "*"
+        echo "indenting increases file size by a factor of $incr"
+        echo "*"
+        echo ""
+
+        test 1 -eq $(( $incr < 10 ))
+        assertEquals "Indenting should not increase file size by more then 10 fold" "0" "$?"
+
+        rm /tmp/p2x-speed-in
+        rm /tmp/p2x-out.xml
+    done
 }
 
 . shunit2
