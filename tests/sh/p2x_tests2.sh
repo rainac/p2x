11ef1872302193da1c8bdffdff88463225002a7b
rainac <johannes@johannes-willkomm.de>
2015-08-12 23:45:39 +0200

    Improve test case

--- tests/sh/p2x_tests2.sh
+++ tests/sh/p2x_tests2.sh
@@ -9,6 +9,13 @@ export LANG=C # for grep used in shunit2, depends on english output
 testP2X1() {
     p2x -p  ../../examples/configs/fort2.conf ../../examples/in/fortran1.exp > log 2> err
     assertEquals "P2X should not fail in this case" "0" "$?"
+    grep -i "unexpected" err > /dev/null
+    res=$?
+    assertEquals "P2X should print and error message" "0" "$?"
+    grep -i "end of input" err > /dev/null
+    res=$?
+    assertEquals "P2X should print and error message" "0" "$?"
+    rm log err
 }
 
 . shunit2
