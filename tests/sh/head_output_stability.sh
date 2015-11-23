88879cef6842decb39b43ee0ee708d1ff6bd7622
rainac <johannes@johannes-willkomm.de>
2015-11-23 21:58:16 +0100

    Amend test: pre-delete output data, no reason to skipt files any more

--- tests/sh/head_output_stability.sh
+++ tests/sh/head_output_stability.sh
@@ -7,14 +7,14 @@ checkExpFile() {
     i=$1
     opts=""
     stored_out=../../examples/out/$(basename $i .exp).xml
-    if test "$i" = "ftp2.exp" || test "$i" = "f1.exp" || test "$i" = "fl1rg.exp" || test "$i" = "1p2ep3.exp"; then
-        return
-    fi
     if test "$i" = "letter.exp" || test "$i" = "german.exp" || test "$i" = "fliesst.exp"; then
         opts="-e latin1"
     fi
     if test -f "$stored_out"; then
         echo "Parse file $i"
+
+        rm -f $tmpdir/res.xml $tmpdir/res2.xml
+        touch $tmpdir/res2.xml
 
         p2x $opts -p ../../examples/configs/default ../../examples/in/$i > $tmpdir/res.xml
         xsltproc -o $tmpdir/res2.xml ../../src/xsl/but-root.xsl $tmpdir/res.xml
