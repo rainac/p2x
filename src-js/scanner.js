3907dc9ea55d3a2e693822d95591f7cea372f241
Johannes Willkomm <johannes@johannes-willkomm.de>
2016-02-26 16:10:12 +0100

    Minify: Automatically change required local .js scripts suffix to .min.js

--- src-js/scanner.js
+++ src-js/scanner.js
@@ -2,7 +2,7 @@
 var P2X = P2X || {};
 
 if (typeof window == 'undefined') {
-    P2X._HashMap = require('./hashmap.min.js')
+    P2X._HashMap = require('./hashmap.js')
     P2X.HashMap = P2X._HashMap.HashMap
 }
 
@@ -25,14 +25,14 @@ if (typeof window == 'undefined') {
     
     var fs = require('fs')
     var mod_assert = require('assert')
-    var pXML = require('./parse-xml.min.js')
+    var pXML = require('./parse-xml.js')
 
     var ENUM = {}
-    ENUM.ParserMode = require('./modes.ncd.min.js')
+    ENUM.ParserMode = require('./modes.ncd.js')
     P2X.importObject(ENUM.ParserMode, ENUM)
-    ENUM.ParserToken = require('./token.ncd.min.js')
+    ENUM.ParserToken = require('./token.ncd.js')
     P2X.importObject(ENUM.ParserToken, ENUM)
-    ENUM.ParserAssoc = require('./assoc.ncd.min.js')
+    ENUM.ParserAssoc = require('./assoc.ncd.js')
     P2X.importObject(ENUM.ParserAssoc, ENUM)
     var assert = function(cond, msg) {
         mod_assert(cond, msg)
