0f9a7bcbb1961d2f4b3d353c7214af4887ce18c5
rainac <johannes@johannes-willkomm.de>
2016-09-04 00:18:02 +0200

    Fix npm calls

--- docker/scripts/debian-install-packages.sh
+++ docker/scripts/debian-install-packages.sh
@@ -2,8 +2,8 @@
 
 apt-get -y install git g++ gcc make automake shunit2 xmlstarlet xsltproc flex libcppunit-dev zsh octave trang npm
 
-npm -g mocha
-npm -g tmp
-npm -g most-xml
+npm install -g mocha
+npm install -g tmp
+npm install -g most-xml
 
 sudo ln -sf /usr/bin/nodejs /usr/local/bin/node
