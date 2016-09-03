f38b5565449c207b60e6d1cd1cb6cb71c6531343
rainac <johannes@johannes-willkomm.de>
2016-09-03 22:48:46 +0200

    Docker scripts reorganized

--- docker/test-on-docker.sh
+++ docker/test-on-docker.sh
@@ -2,10 +2,16 @@
 
 cont=$1
 
-docker exec -u root -i $cont /bin/bash < debian-install-packages.sh || exit 1
-docker exec -i $cont git reset --hard || exit 1
-docker exec -i $cont git clean -x -f -d || exit 1
-docker exec -i $cont git pull || exit 1
-docker exec -i $cont /bin/bash < run-configure.sh || exit 1
-docker exec -i $cont /bin/bash < compile-and-install.sh || exit 1
-docker exec -i $cont /bin/bash < run-make-check.sh || exit 1
+WD=/home/devel/
+BRANCH=${2:-master}
+
+docker cp scripts $cont:$WD/scripts
+
+docker exec -u root -it $cont $WD/scripts/debian-install-packages.sh || exit 1
+docker exec -it $cont git reset --hard || exit 1
+docker exec -it $cont git clean -x -f -d || exit 1
+docker exec -it $cont git checkout $BRANCH || exit 1
+docker exec -it $cont git pull || exit 1
+docker exec -it $cont $WD/scripts/run-configure.sh || exit 1
+docker exec -it $cont $WD/scripts/compile-and-install.sh || exit 1
+docker exec -it $cont $WD/scripts/run-make-check.sh || exit 1
