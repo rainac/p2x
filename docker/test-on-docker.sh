38562bb7144da6d9948031bbb1df4f77e5e1d716
rainac <johannes@johannes-willkomm.de>
2016-09-03 22:52:49 +0200

    Another tweak

--- docker/test-on-docker.sh
+++ docker/test-on-docker.sh
@@ -7,11 +7,16 @@ BRANCH=${2:-master}
 
 docker cp scripts $cont:$WD/scripts
 
-docker exec -u root -it $cont $WD/scripts/debian-install-packages.sh || exit 1
-docker exec -it $cont git reset --hard || exit 1
-docker exec -it $cont git clean -x -f -d || exit 1
-docker exec -it $cont git checkout $BRANCH || exit 1
-docker exec -it $cont git pull || exit 1
-docker exec -it $cont $WD/scripts/run-configure.sh || exit 1
-docker exec -it $cont $WD/scripts/compile-and-install.sh || exit 1
-docker exec -it $cont $WD/scripts/run-make-check.sh || exit 1
+dopts=-i
+if test -t 0; then
+    dopts="$dopts -t"
+fi
+
+docker exec -u root $dopts $cont $WD/scripts/debian-install-packages.sh || exit 1
+docker exec $dopts $cont git reset --hard || exit 1
+docker exec $dopts $cont git clean -x -f -d || exit 1
+docker exec $dopts $cont git checkout $BRANCH || exit 1
+docker exec $dopts $cont git pull || exit 1
+docker exec $dopts $cont $WD/scripts/run-configure.sh || exit 1
+docker exec $dopts $cont $WD/scripts/compile-and-install.sh || exit 1
+docker exec $dopts $cont $WD/scripts/run-make-check.sh || exit 1
