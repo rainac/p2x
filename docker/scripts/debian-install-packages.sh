16b909a08f058f57d9ede98291669f18c48c94fa
rainac <johannes@johannes-willkomm.de>
2016-09-04 12:11:34 +0200

    npm commands removed from this script

--- docker/scripts/debian-install-packages.sh
+++ docker/scripts/debian-install-packages.sh
@@ -2,8 +2,4 @@
 
 apt-get -y install git g++ gcc make automake shunit2 xmlstarlet xsltproc flex libcppunit-dev zsh octave trang npm
 
-npm install -g mocha
-npm install -g tmp
-npm install -g most-xml
-
 sudo ln -sf /usr/bin/nodejs /usr/local/bin/node
