5eeb05d53c78419287f329b95e3cbd10866d4130
Johannes Willkomm <johannes@johannes-willkomm.de>
2016-10-11 00:42:44 +0200

    Another test

--- tests/sh/output_modey.sh
+++ tests/sh/output_modey.sh
@@ -32,10 +32,23 @@ testReproduce() {
         saving=$(( $sz1 / $sz2 ))
         echo "saved: $sz1 / $sz2 = $saving"
 
-        rm res.xml res.txt
-
         test $saving -gt 0
         assertEquals "Alternate XML format should be smaller or of same size" 0 $?
+
+        p2x --indent $opts $alt_opts -p ../../examples/configs/default $i > res.xml
+        xsltproc ../../src/xsl/$reprxsl res.xml > res.txt
+        diff $i res.txt
+        assertEquals "Alternate opts reproduce test $i did not return same result" 0 $?
+
+        sz3=$(ls -l res.xml | cut -d " " -f 5)
+        saving=$(( $sz2 / $sz3 ))
+        echo "saved: $sz2 / $sz3 = $saving"
+
+        test $saving -gt 0
+        assertEquals "Alternate XML format without indent should be even smaller or of same size" 0 $?
+
+        rm res.xml res.txt
+
     done
 
 }
