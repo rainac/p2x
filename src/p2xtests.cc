aac759e2199c78c0d4678906e84704bbbc7a650b
Johannes Willkomm <johannes@johannes-willkomm.de>
2016-10-14 08:48:35 +0200

    flags removed

--- src/p2xtests.cc
+++ src/p2xtests.cc
@@ -32,7 +32,6 @@ struct TokenTests  : public CppUnit::TestFixture {
     CPPUNIT_ASSERT(t.line == 0);
     CPPUNIT_ASSERT(t.column == 0);
     CPPUNIT_ASSERT(t.character == 0);
-    CPPUNIT_ASSERT(t.flags == 0);
     CPPUNIT_ASSERT(t.text == "");
   }
   
