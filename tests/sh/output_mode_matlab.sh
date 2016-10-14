9fe6476a0492bd034281cd59eac0c3c57531a92a
Johannes Willkomm <johannes@johannes-willkomm.de>
2016-10-15 00:18:43 +0200

    Add test

--- tests/sh/output_mode_matlab.sh
+++ tests/sh/output_mode_matlab.sh
@@ -46,6 +46,21 @@ test_ReproduceStrict() {
 
 }
 
+test_ReproduceIndent() {
+
+    alt_opts_="$alt_opts --indent"
+
+    echo "1: Parse files with $common_opts"
+    echo "2: Parse files with $common_opts $alt_opts_"
+
+    for i in ../../examples/in/*.exp; do
+        #        echo "Parse test file $i"
+        ReproduceMatlab $i $alt_opts_ $common_opts
+    done
+    echo ""
+
+}
+
 test_QuitOctave() {
 
     echo "quit" >&p
