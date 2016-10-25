6fc4fb451c70ba9a4e56bde104ecfacbc6391eb6
Johannes Willkomm <johannes@johannes-willkomm.de>
2016-10-25 22:37:37 +0200

    Output some more information

--- tests/sh/transformation_speed.sh
+++ tests/sh/transformation_speed.sh
@@ -7,13 +7,15 @@ export SHUNIT_PARENT=$0
 export LANG=C # for grep used in shunit2, depends on english output
 TMP=${TMP:-/tmp}
 
+P2XOPTS=(-Vtimes,io)
+
 test_LargeCVS1_create() {
     yes Abc,123,456.789 | head -n 1000000 > $TMP/large.csv
     sz1=$(ls -l $TMP/large.csv | cut -d " " -f 5)
 }
 
 test_LargeCVS1_parse2XML_addcols_xslt() {
-    time p2x --indent -m -bNEWLINE -bCOMMA -iSPACE -o res.xml $TMP/large.csv
+    time p2x $P2XOPTS --indent -m -bNEWLINE -bCOMMA -iSPACE -o res.xml $TMP/large.csv
     assertEquals "P2X should not fail in this case" 0 $?
     sz2=$(ls -l res.xml | cut -d " " -f 5)
     ratio=$(( 1.0 * $sz2 / $sz1 ))
@@ -23,7 +25,7 @@ test_LargeCVS1_parse2XML_addcols_xslt() {
 }
 
 test_LargeCVS1_parse2XML_Y_addcols_xslt() {
-    time p2x --indent -m --output-mode=y -bNEWLINE -bCOMMA -iSPACE -o res.xml $TMP/large.csv
+    time p2x $P2XOPTS --indent -m --output-mode=y -bNEWLINE -bCOMMA -iSPACE -o res.xml $TMP/large.csv
     assertEquals "P2X should not fail in this case" 0 $?
     sz2=$(ls -l res.xml | cut -d " " -f 5)
     ratio=$(( 1.0 * $sz2 / $sz1 ))
@@ -33,7 +35,7 @@ test_LargeCVS1_parse2XML_Y_addcols_xslt() {
 }
 
 test_LargeCVS1_parse2JSON_addcols_py() {
-    time p2x --indent -m -J -bNEWLINE -bCOMMA -iSPACE -o res.json $TMP/large.csv
+    time p2x $P2XOPTS --indent -m -J -bNEWLINE -bCOMMA -iSPACE -o res.json $TMP/large.csv
     assertEquals "P2X should not fail in this case" 0 $?
     sz2=$(ls -l res.json | cut -d " " -f 5)
     ratio=$(( 1.0 * $sz2 / $sz1 ))
