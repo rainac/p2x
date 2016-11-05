236ce37a1801a87b614241f9839587f56e97584a
Johannes Willkomm <johannes@johannes-willkomm.de>
2016-11-05 21:25:00 +0100

    Enable tests in parallel

--- tests/sh/common.sh
+++ tests/sh/common.sh
@@ -4,6 +4,8 @@ zmodload zsh/mathfunc
 set -o shwordsplit
 export LANG=C # for grep used in shunit2, depends on english output
 
+. ./setup_tmp.sh
+
 trapSCHLD() {
     echo "SIGCHILD received"
 }
@@ -65,28 +67,27 @@ ReproduceTest() {
 
     opts="$eopts $arg1_opts"
     echo -n "Parse file $infile with '$opts', "
-    p2x $opts -p ../../examples/configs/default $infile > res.xml
-    xsltproc ../../src/xsl/$reprxsl res.xml > res.txt
-    diff $infile res.txt > /dev/null
+    p2x $opts -p ../../examples/configs/default $infile > $tmpdir/res.xml
+    xsltproc ../../src/xsl/$reprxsl $tmpdir/res.xml > $tmpdir/res.txt
+    diff $infile $tmpdir/res.txt > /dev/null
     assertEquals "Plain reproduce test $infile did not return same result" 0 $?
 
-    sz1=$(ls -l res.xml | cut -d " " -f 5)
+    sz1=$(ls -l $tmpdir/res.xml | cut -d " " -f 5)
 
     opts=($eopts $arg1_opts $arg1_alt_opts)
     echo -n "Parse file $infile with '$opts'\r"
-    p2x $opts -p ../../examples/configs/default $infile > res.xml
-    xsltproc ../../src/xsl/$reprxsl res.xml > res.txt
-    diff $infile res.txt
+    p2x $opts -p ../../examples/configs/default $infile > $tmpdir/res.xml
+    xsltproc ../../src/xsl/$reprxsl $tmpdir/res.xml > $tmpdir/res.txt
+    diff $infile $tmpdir/res.txt
     assertEquals "Alternate opts reproduce test $infile did not return same result" 0 $?
 
-    sz2=$(ls -l res.xml | cut -d " " -f 5)
+    sz2=$(ls -l $tmpdir/res.xml | cut -d " " -f 5)
     saving=$(( 1.0 * $sz1 / $sz2 ))
 #    echo "saved: $sz1 / $sz2 = $saving"
 
     [[ $saving -gt 1 ]]
 #    assertEquals "Alternate XML format should be smaller or of same size: ($sz1 > $sz2)" 0 $?
 
-    rm res.xml res.txt
 }
 
 
@@ -104,51 +105,49 @@ ReproduceMatlab() {
 
     opts="$eopts $arg1_opts"
     echo -n "Parse with '$opts': file $infile"
-    p2x $p2xopts $opts -p ../../examples/configs/default $infile > res.m
-    cat > runscript.m <<EOF
-run('res.m');
-fd = fopen('res.txt', 'w');
+    p2x $p2xopts $opts -p ../../examples/configs/default $infile > $tmpdir/res.m
+    cat > $tmpdir/runscript.m <<EOF
+run('$tmpdir/res.m');
+fd = fopen('$tmpdir/res.txt', 'w');
 fprintf(fd, '%s', reproduce(ans));
 fclose(fd);
 'done'
 EOF
 #    octave --eval runscript
-    cat runscript.m >&p
+    cat $tmpdir/runscript.m >&p
     read -p FFF
 #    echo "oct>> $FFF"
     assertEquals "Output should be valid MATLAB code" 0 $?
-    diff $diffopts $infile res.txt
+    diff $diffopts $infile $tmpdir/res.txt
     assertEquals "Plain reproduce test $infile did not return same result" 0 $?
 
-    sz1=$(ls -l res.m | cut -d " " -f 5)
+    sz1=$(ls -l $tmpdir/res.m | cut -d " " -f 5)
 
     opts=($eopts $arg1_opts $arg1_alt_opts)
     echo -n "Parse with '$opts': file $infile"
-    p2x $p2xopts $opts -p ../../examples/configs/default $infile > res2.m
-    cat > runscript.m <<EOF
-run('res2.m');
-fd = fopen('res.txt', 'w');
+    p2x $p2xopts $opts -p ../../examples/configs/default $infile > $tmpdir/res2.m
+    cat > $tmpdir/runscript.m <<EOF
+run('$tmpdir/res2.m');
+fd = fopen('$tmpdir/res.txt', 'w');
 fprintf(fd, '%s', reproduce(ans));
 fclose(fd);
 'done'
 EOF
 #    octave --eval runscript
-    cat runscript.m >&p
+    cat $tmpdir/runscript.m >&p
     read -p FFF
 #    echo "oct>> $FFF"
     assertEquals "Output should be valid MATLAB code" 0 $?
-    diff $diffopts $infile res.txt
+    diff $diffopts $infile $tmpdir/res.txt
     assertEquals "Alternate opts reproduce test $infile did not return same result" 0 $?
     echo -n "\r"
 
-    sz2=$(ls -l res2.m | cut -d " " -f 5)
+    sz2=$(ls -l $tmpdir/res2.m | cut -d " " -f 5)
     saving=$(( 1.0 * $sz1 / $sz2 ))
 #    echo "saved: $sz1 / $sz2 = $saving"
 
     [[ $saving -ge 1 ]]
 #    assertEquals "Alternate XML format should be smaller or of same size: ($sz1 > $sz2)" 0 $?
-
-    rm res.txt res.m res2.m runscript.m
 
 }
 
@@ -174,30 +173,28 @@ ReproduceJSONPy() {
 
     opts="$eopts $arg1_opts"
     echo -n "Parse with '$opts': file $infile"
-    p2x $p2xopts $opts -p ../../examples/configs/default $infile > res.json
-    python $PWD/../../src/py/reproduce.py < res.json > res.txt
+    p2x $p2xopts $opts -p ../../examples/configs/default $infile > $tmpdir/res.json
+    python $PWD/../../src/py/reproduce.py < $tmpdir/res.json > $tmpdir/res.txt
     assertEquals "Output should be valid MATLAB code" 0 $?
-    diff $diffopts $infile res.txt
+    diff $diffopts $infile $tmpdir/res.txt
     assertEquals "Plain reproduce test $infile did not return same result" 0 $?
 
-    sz1=$(ls -l res.json | cut -d " " -f 5)
+    sz1=$(ls -l $tmpdir/res.json | cut -d " " -f 5)
 
     opts=($eopts $arg1_opts $arg1_alt_opts)
     echo -n "Parse with '$opts': file $infile"
-    p2x $p2xopts $opts -p ../../examples/configs/default $infile > res2.json
-    python $PWD/../../src/py/reproduce.py < res2.json > res2.txt
+    p2x $p2xopts $opts -p ../../examples/configs/default $infile > $tmpdir/res2.json
+    python $PWD/../../src/py/reproduce.py < $tmpdir/res2.json > $tmpdir/res2.txt
     assertEquals "Output should be valid MATLAB code" 0 $?
-    diff $diffopts $infile res2.txt
+    diff $diffopts $infile $tmpdir/res2.txt
     assertEquals "Alternate opts reproduce test $infile did not return same result" 0 $?
     echo -n "\r"
 
-    sz2=$(ls -l res2.json | cut -d " " -f 5)
+    sz2=$(ls -l $tmpdir/res2.json | cut -d " " -f 5)
     saving=$(( 1.0 * $sz1 / $sz2 ))
 #    echo "saved: $sz1 / $sz2 = $saving"
 
     [[ $saving -ge 1 ]]
 #    assertEquals "Alternate XML format should be smaller or of same size: ($sz1 > $sz2)" 0 $?
-
-    rm res.txt res2.txt res.json res2.json
 
 }
