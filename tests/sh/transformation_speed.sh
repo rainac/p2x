5b06b32296fcb4f37656991ada269683f79f75f6
Johannes Willkomm <johannes@johannes-willkomm.de>
2016-11-05 21:04:18 +0100

    move grammar def to own file here

--- tests/sh/transformation_speed.sh
+++ tests/sh/transformation_speed.sh
@@ -15,7 +15,7 @@ test_LargeCVS1_create() {
 }
 
 test_LargeCVS1_parse2XML_addcols_xslt() {
-    time p2x $P2XOPTS --indent -m -bNEWLINE -bCOMMA -iSPACE -o res.xml $TMP/large.csv
+    time p2x $P2XOPTS --indent -m -p ../../examples/configs/csv -o res.xml $TMP/large.csv
     assertEquals "P2X should not fail in this case" 0 $?
     sz2=$(ls -l res.xml | cut -d " " -f 5)
     ratio=$(( 1.0 * $sz2 / $sz1 ))
@@ -25,7 +25,7 @@ test_LargeCVS1_parse2XML_addcols_xslt() {
 }
 
 test_LargeCVS1_parse2XML_Y_addcols_xslt() {
-    time p2x $P2XOPTS --indent -m --output-mode=y -bNEWLINE -bCOMMA -iSPACE -o res.xml $TMP/large.csv
+    time p2x $P2XOPTS --indent -m --output-mode=y -p ../../examples/configs/csv -o res.xml $TMP/large.csv
     assertEquals "P2X should not fail in this case" 0 $?
     sz2=$(ls -l res.xml | cut -d " " -f 5)
     ratio=$(( 1.0 * $sz2 / $sz1 ))
@@ -35,7 +35,7 @@ test_LargeCVS1_parse2XML_Y_addcols_xslt() {
 }
 
 test_LargeCVS1_parse2JSON_addcols_py() {
-    time p2x $P2XOPTS --indent -m -J -bNEWLINE -bCOMMA -iSPACE -o res.json $TMP/large.csv
+    time p2x $P2XOPTS --indent -m -J -p ../../examples/configs/csv -o res.json $TMP/large.csv
     assertEquals "P2X should not fail in this case" 0 $?
     sz2=$(ls -l res.json | cut -d " " -f 5)
     ratio=$(( 1.0 * $sz2 / $sz1 ))
