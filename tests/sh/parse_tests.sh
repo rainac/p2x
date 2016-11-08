3e09ba4988048848f0bc3714336cf8aa159920de
rainac <johannes@johannes-willkomm.de>
2016-11-08 23:39:28 +0100

    Amend test

--- tests/sh/parse_tests.sh
+++ tests/sh/parse_tests.sh
@@ -9,12 +9,12 @@ testParseTreeEqual1() {
 }
 
 testParseTreeEqual2() {
-  env -i  P2XFLAGS=-m  ./parse_tests_same_when_merged.sh
+  env -i               ./parse_tests_not_same_when_merged.sh
   assertEquals "sub test should pass" "0" "$?"
 }
 
 testParseTreeEqual3() {
-  env -i               ./parse_tests_not_same_when_merged.sh
+  env -i  P2XFLAGS=-m  ./parse_tests_same_when_merged.sh
   assertEquals "sub test should pass" "0" "$?"
 }
 
