96655243901c652d266893bb8dc5ca457da013c0
Johannes Willkomm <johannes@johannes-willkomm.de>
2018-10-05 20:32:43 +0200

    Correct test condition

--- tests/sh/p2x_tests2.sh
+++ tests/sh/p2x_tests2.sh
@@ -33,7 +33,7 @@ testP2X_fail_fortran2() {
 testP2X_no_output_newline() {
     p2x --indent -p  ../../examples/configs/default ../../examples/in/email.exp > res.xml 2> err
     assertEquals "P2X should not fail in this case" "0" "$?"
-    numl=$(wc res.xml | awk '{print $1}')
+    numl=$(grep -v -e -- res.xml | wc | awk '{print $1}')
     test $numl = 2
     assertEquals "P2X should print no newlines in XML by default" "0" "$?"
     rm err res.xml
@@ -43,7 +43,7 @@ testP2X_no_output_newline() {
 testP2X_output_newline_if_desired() {
     p2x --indent --newline-as-br -p  ../../examples/configs/default ../../examples/in/email.exp > res.xml 2> err
     assertEquals "P2X should not fail in this case" "0" "$?"
-    numl=$(wc res.xml | awk '{print $1}')
+    numl=$(grep -v -e -- res.xml | wc | awk '{print $1}')
     test $numl = 6
     assertEquals "P2X should print newlines in XML if desired" "0" "$?"
     rm err res.xml
@@ -52,7 +52,7 @@ testP2X_output_newline_if_desired() {
 testP2X_output_newline_as_entity_if_desired() {
     p2x --indent --newline-as-entity -p  ../../examples/configs/default ../../examples/in/email.exp > res.xml 2> err
     assertEquals "P2X should not fail in this case" "0" "$?"
-    numl=$(wc res.xml | awk '{print $1}')
+    numl=$(grep -v -e -- res.xml | wc | awk '{print $1}')
     test $numl = 2
     assertEquals "P2X should print no newlines in XML when desired" "0" "$?"
     grep -E "&#xa;" res.xml > /dev/null
