9525043817be9077c9a50bd15f44acdfa70bf98b
Johannes Willkomm <johannes@johannes-willkomm.de>
2020-03-13 22:55:01 +0100

    Tests added

--- tests/sh/parse_tests_null_placement.sh
+++ tests/sh/parse_tests_null_placement.sh
@@ -22,6 +22,25 @@ checkParseTreeEqual() {
     assertEquals "Parse tree is not in expected form" "$exp" "$res"
 }
 
+checkParseTreeEqual2() {
+    infile=$1
+    exp=$2
+    opts="$3"
+    config="${4:-../../examples/configs/default}"
+    p2x $opts -p $config ../../examples/in/$infile > res.xml
+    xmlstarlet el res.xml > res.txt
+    res=$(cat res.txt)
+    echo "$infile: $(cat ../../examples/in/$infile)  => res.txt"
+    if [[ "$exp" != "$res" ]]; then
+        echo "**** soll ****"
+        echo $exp
+        echo "**** result ****"
+        echo $res
+        echo "****  ****"
+    fi
+    assertEquals "Parse tree is not in expected form" "$exp" "$res"
+}
+
 testParseTreeEqual_null_in_merged1() {
     checkParseTreeEqual miss-ops1.exp '[NEWLINE]([COMMA](., ., x, ., .))' '' '../../examples/configs/merged-binary'
 }
@@ -35,17 +54,93 @@ testParseTreeEqual_null_in_merged4() {
     checkParseTreeEqual miss-ops4.exp '[NEWLINE]([COLON](., [COLON](., [COLON](x, [COLON]()))))' '' '../../examples/configs/merged-binary'
 }
 
-teestParseTreeEqual_null_in_merged1_X() {
-    checkParseTreeEqual miss-ops1.exp '' '-X' '../../examples/configs/merged-binary'
+testParseTreeEqual_null_in_merged1_X() {
+    read -r -d '' VAR <<'EOF'
+code-xml
+code-xml/c:version
+code-xml/ROOT
+code-xml/ROOT/null
+code-xml/ROOT/NEWLINE
+code-xml/ROOT/NEWLINE/COMMA
+code-xml/ROOT/NEWLINE/COMMA/null
+code-xml/ROOT/NEWLINE/COMMA/c:t
+code-xml/ROOT/NEWLINE/COMMA/null
+code-xml/ROOT/NEWLINE/COMMA/c:t
+code-xml/ROOT/NEWLINE/COMMA/ID
+code-xml/ROOT/NEWLINE/COMMA/ID/c:t
+code-xml/ROOT/NEWLINE/COMMA/c:t
+code-xml/ROOT/NEWLINE/COMMA/null
+code-xml/ROOT/NEWLINE/COMMA/c:t
+code-xml/ROOT/NEWLINE/COMMA/null
+code-xml/ROOT/NEWLINE/c:br
+EOF
+    checkParseTreeEqual2 miss-ops1.exp "$VAR" '-X' '../../examples/configs/merged-binary'
 }
-teestParseTreeEqual_null_in_merged2_X() {
-    checkParseTreeEqual miss-ops2.exp '[NEWLINE]([FULL_STOP]([FULL_STOP]([FULL_STOP]([FULL_STOP](), x))))' '-X' '../../examples/configs/merged-binary'
+testParseTreeEqual_null_in_merged2_X() {
+    read -r -d '' VAR <<'EOF'
+code-xml
+code-xml/c:version
+code-xml/ROOT
+code-xml/ROOT/null
+code-xml/ROOT/NEWLINE
+code-xml/ROOT/NEWLINE/FULL_STOP
+code-xml/ROOT/NEWLINE/FULL_STOP/FULL_STOP
+code-xml/ROOT/NEWLINE/FULL_STOP/FULL_STOP/FULL_STOP
+code-xml/ROOT/NEWLINE/FULL_STOP/FULL_STOP/FULL_STOP/FULL_STOP
+code-xml/ROOT/NEWLINE/FULL_STOP/FULL_STOP/FULL_STOP/FULL_STOP/c:t
+code-xml/ROOT/NEWLINE/FULL_STOP/FULL_STOP/FULL_STOP/c:t
+code-xml/ROOT/NEWLINE/FULL_STOP/FULL_STOP/FULL_STOP/ID
+code-xml/ROOT/NEWLINE/FULL_STOP/FULL_STOP/FULL_STOP/ID/c:t
+code-xml/ROOT/NEWLINE/FULL_STOP/FULL_STOP/c:t
+code-xml/ROOT/NEWLINE/FULL_STOP/c:t
+code-xml/ROOT/NEWLINE/c:br
+EOF
+    checkParseTreeEqual2 miss-ops2.exp "$VAR" '-X' '../../examples/configs/merged-binary'
 }
-teestParseTreeEqual_null_in_merged3_X() {
-    checkParseTreeEqual miss-ops3.exp '' '-X' '../../examples/configs/merged-binary'
+testParseTreeEqual_null_in_merged3_X() {
+    read -r -d '' VAR <<'EOF'
+code-xml
+code-xml/c:version
+code-xml/ROOT
+code-xml/ROOT/null
+code-xml/ROOT/NEWLINE
+code-xml/ROOT/NEWLINE/SEMICOLON
+code-xml/ROOT/NEWLINE/SEMICOLON/null
+code-xml/ROOT/NEWLINE/SEMICOLON/c:t
+code-xml/ROOT/NEWLINE/SEMICOLON/null
+code-xml/ROOT/NEWLINE/SEMICOLON/c:t
+code-xml/ROOT/NEWLINE/SEMICOLON/ID
+code-xml/ROOT/NEWLINE/SEMICOLON/ID/c:t
+code-xml/ROOT/NEWLINE/SEMICOLON/c:t
+code-xml/ROOT/NEWLINE/SEMICOLON/null
+code-xml/ROOT/NEWLINE/SEMICOLON/c:t
+code-xml/ROOT/NEWLINE/SEMICOLON/null
+code-xml/ROOT/NEWLINE/c:br
+EOF
+    checkParseTreeEqual2 miss-ops3.exp "$VAR" '-X' '../../examples/configs/merged-binary'
 }
-teestParseTreeEqual_null_in_merged4_X() {
-    checkParseTreeEqual miss-ops4.exp '[NEWLINE]([COLON](., [COLON](., [COLON](x, [COLON]()))))' '-X' '../../examples/configs/merged-binary'
+testParseTreeEqual_null_in_merged4_X() {
+    read -r -d '' VAR <<'EOF'
+code-xml
+code-xml/c:version
+code-xml/ROOT
+code-xml/ROOT/null
+code-xml/ROOT/NEWLINE
+code-xml/ROOT/NEWLINE/COLON
+code-xml/ROOT/NEWLINE/COLON/null
+code-xml/ROOT/NEWLINE/COLON/c:t
+code-xml/ROOT/NEWLINE/COLON/COLON
+code-xml/ROOT/NEWLINE/COLON/COLON/null
+code-xml/ROOT/NEWLINE/COLON/COLON/c:t
+code-xml/ROOT/NEWLINE/COLON/COLON/COLON
+code-xml/ROOT/NEWLINE/COLON/COLON/COLON/ID
+code-xml/ROOT/NEWLINE/COLON/COLON/COLON/ID/c:t
+code-xml/ROOT/NEWLINE/COLON/COLON/COLON/c:t
+code-xml/ROOT/NEWLINE/COLON/COLON/COLON/COLON
+code-xml/ROOT/NEWLINE/COLON/COLON/COLON/COLON/c:t
+code-xml/ROOT/NEWLINE/c:br
+EOF
+    checkParseTreeEqual2 miss-ops4.exp "$VAR" '-X' '../../examples/configs/merged-binary'
 }
 
 . ./myshunit2
