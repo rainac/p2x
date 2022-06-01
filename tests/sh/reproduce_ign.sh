c357a063575c743030b93d7c1c1a11ec77bc82d0
Johannes Willkomm <johannes@johannes-willkomm.de>
2022-06-01 21:41:49 +0200

    Another JS test suite added

--- tests/sh/reproduce_ign.sh
+++ tests/sh/reproduce_ign.sh
@@ -2,14 +2,14 @@
 
 mydir=$(dirname $BASH_SOURCE)
 
-. $mydir/setup_tmp.sh
+. $mydir/setup.sh
 
 checkExpFile() {
     i=$1
     echo "Parse file $i"
     opts=""
     reprxsl=reproduce.xsl
-    p2xjs $opts -S $mydir/../../examples/configs/scanner-simple.json -p $tmpdir/conf-with-ignores $mydir/../../examples/in/$i > $tmpdir/res.xml
+    p2xjs $opts -S $mydir/../../examples/configs/scanner-c.json -p $tmpdir/conf-with-ignores $mydir/../../examples/in/$i > $tmpdir/res.xml
     xsltproc $mydir/../../src/xsl/$reprxsl $tmpdir/res.xml > $tmpdir/res.txt
     diff $mydir/../../examples/in/$i $tmpdir/res.txt > /dev/null
     assertEquals "Reproduce test $i did not return same result" 0 $?
